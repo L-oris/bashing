412c467f39811416ef1f4b4eeb01cf082e922c15
Loris <loris4526@gmail.com>
2018-12-08 09:48:30 +0100

    add-new-local-user.sh

---
+++ 2-add-new-local-user.sh
@@ -0,0 +1,52 @@
+#!/bin/env bash
+#
+# This script creates a new user on the local system
+# You must supply a username as an argument to the script
+# The remaining arguments will be treated as the full name for the account
+# An initial password will be automatically generated for the account
+# The new account forces password change on first login
+
+if [[ "$(id -u)" != 0 ]]; then
+    echo 'Please run with sudo or as root'
+    exit 1
+fi
+
+if [[ "$#" -lt 1 ]]; then 
+    echo "Usage: $(basename $0) USER_NAME [COMMENT]..."
+    echo 'Create an account on the local system with the name of USER_NAME and a comments field of COMMENT'
+    exit 1
+fi
+
+# The first parameter is the user name
+USER_NAME=$1
+# The rest of the parameters are for the account comments
+shift
+COMMENT="$@"
+
+PASSWORD=$(date +%s%N${RANDOM} | sha256sum | head -c48)
+
+useradd --comment "$COMMENT" --create-home "$USER_NAME"
+if [[ "$?" -ne 0 ]]; then
+    echo 'Cannot create account'
+    exit 1
+fi
+
+echo "$PASSWORD" | passwd --stdin "$USER_NAME"
+if [[ "$?" -ne 0 ]]; then
+    echo 'Cannot set password for account'
+    exit 1
+fi
+
+passwd --expire "$USER_NAME"
+
+cat << _EOF_
+
+User Successfully Created:
+* Username: ${USER_NAME}
+* Full Name: ${COMMENT}
+* Password: ${PASSWORD}
+* Hostname: ${HOSTNAME}
+
+_EOF_
+
+exit 0
