eea4b4814d6f96d14d8b80dfb5f05328da64b886
Loris <loris4526@gmail.com>
2018-12-10 08:16:36 +0100

    redirect to stderr, and send unused output to null device

--- 2-add-new-local-user.sh
+++ 2-add-new-local-user.sh
@@ -7,13 +7,13 @@
 # The new account forces password change on first login
 
 if [[ "$(id -u)" != 0 ]]; then
-    echo 'Please run with sudo or as root'
+    echo 'Please run with sudo or as root' >&2
     exit 1
 fi
 
 if [[ "$#" -lt 1 ]]; then 
-    echo "Usage: $(basename $0) USER_NAME [COMMENT]..."
-    echo 'Create an account on the local system with the name of USER_NAME and a comments field of COMMENT'
+    echo "Usage: $(basename $0) USER_NAME [COMMENT]..." >&2
+    echo 'Create an account on the local system with the name of USER_NAME and a comments field of COMMENT' >&2
     exit 1
 fi
 
@@ -25,28 +25,26 @@ COMMENT="$@"
 
 PASSWORD=$(date +%s%N${RANDOM} | sha256sum | head -c48)
 
-useradd --comment "$COMMENT" --create-home "$USER_NAME"
+useradd --comment "$COMMENT" --create-home "$USER_NAME" &> /dev/null
 if [[ "$?" -ne 0 ]]; then
-    echo 'Cannot create account'
+    echo 'Cannot create account' >&2
     exit 1
 fi
 
-echo "$PASSWORD" | passwd --stdin "$USER_NAME"
+echo "$PASSWORD" | passwd --stdin "$USER_NAME" &> /dev/null
 if [[ "$?" -ne 0 ]]; then
-    echo 'Cannot set password for account'
+    echo 'Cannot set password for account' >&2
     exit 1
 fi
 
-passwd --expire "$USER_NAME"
+passwd --expire "$USER_NAME" &> /dev/null
 
 cat << _EOF_
-
 User Successfully Created:
 * Username: ${USER_NAME}
 * Full Name: ${COMMENT}
 * Password: ${PASSWORD}
 * Hostname: ${HOSTNAME}
-
 _EOF_
 
 exit 0
