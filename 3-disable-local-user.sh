4a17695440da848d53b31704033270f590ea36e1
Loris <loris4526@gmail.com>
2018-12-19 08:08:09 +0100

    clean up for loop for options; functions still to be implemented

--- 3-disable-local-user.sh
+++ 3-disable-local-user.sh
@@ -3,69 +3,56 @@
 # This script disables an account
 
 usage(){
-    echo "Usage: $(basename $0) USER_NAME" >&2
+    echo "Usage: $(basename $0) [OPTIONS..] USER_NAME" >&2
     exit 1
 }
 
-if [[ $(id -u) != '0' ]]; then
-    echo 'Please run as root or super user' >&2
-    usage
+if [[ $(id -u) != 0 ]]; then
+    echo 'Please run with sudo or as root' >&2
+    exit 1
 fi
 
-while getopts adr OPTION; do
+while getopts ard OPTION; do
     case $OPTION in
         a) ARCHIVE_HOME_DIRECTORY='true' ;;
-        d) REMOVE_HOME_DIRECTORY='true' ;;
+        r) REMOVE_HOME_DIRECTORY='true' ;;
         d) DELETE_USER='true' ;;
         ?) usage ;;
     esac
 done
 
-# Shift options
+# Remove options processed by 'getopts'
 shift "$(( OPTIND - 1 ))"
 
-if [[ "$REMOVE_HOME_DIRECTORY" = 1 && "$DELETE_ACCOUNT" != '1' ]]; then
-    echo 'Cannot remove home directory without deleting the account' >&2
+
+if [[ "$#" < 1 ]]; then
     usage
 fi
 
+for USER_NAME in "$@"; do
+    echo "Processing user: ${USER_NAME}.."
 
-if [[ "$#" = 0 ]]; then
-    echo 'Please provide a list of users!' >&2
-    usage
-fi
-
-while [[ "$1" != '' ]]; do
-    USER_NAME=$1
-    shift
-
-    if ! id $USER_NAME >/dev/null 2>&1; then
-        echo "User $USER_NAME does not exist" >&2
-        usage
+    if ! id "$USER_NAME" >/dev/null 2>&1; then
+        echo 'User not found' >&2
+        exit 1
     fi
 
-    if [[ "$(id -u $USER_NAME)" < 1000 ]]; then
-        echo 'Cannot change system accounts' >&2
-        usage
+    USER_ID="$(id -u $USER_NAME)"
+    if [[ "$USER_ID" < 1000 ]]; then
+        echo "Cannot process user $USER_NAME with UID ${USER_ID}: system account" >&2
+        exit 1
     fi
 
-    if [[ "$DELETE_ACCOUNT" != 1 ]]; then
-        disable_account $USER_NAME
-        exit 0
-    
-    if [[ "$CREATE_HOME_DIRECTORY_ARCHIVE" = 1 ]]; then
-        create_home_archive $USER_NAME
-        exit 0
+    if [[ "$ARCHIVE_HOME_DIRECTORY" = 'true' ]]; then
+        archive_home_directory $USER_NAME
     fi
 
-    if [[ "$REMOVE_HOME_DIRECTORY" = 1 ]]; then
-        remove_home_archive_and_delete $USER_NAME
-    else 
-        create_home_archive $USER_NAME
+    if [[ "$DELETE_USER" = 'true' ]]; then
+        delete_user $REMOVE_HOME_DIRECTORY $USER_NAME
+    else
+        disable_user $USER_NAME
     fi
 done
 
 exit 0
-
-# chage -E 0 
 
