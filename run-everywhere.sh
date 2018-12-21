8df2fb29530e13bac42f8cf0234d38edb67d9691
Loris <loris4526@gmail.com>
2018-12-21 09:22:48 +0100

    enable 'DRY_RUN' & 'VERBOSE_MODE'

--- run-everywhere.sh
+++ run-everywhere.sh
@@ -4,6 +4,7 @@
 
 SERVER_LIST='/vagrant/servers'
 VERBOSE_MODE='false'
+DRY_RUN='false'
 
 ##### FUNCTIONS #####
 
@@ -14,29 +15,26 @@ usage(){
 
 ##### MAIN #####
 
-while getopts f:nsv OPTION; do
+if [[ "$(id -u)" = 0 ]]; then
+    echo 'Please do not run command as super user; use -s option instead' >&2
+    exit 1
+fi
+
+while getopts f:dsv OPTION; do
     case $OPTION in
-        f)
-            SERVER_LIST="$OPTARG"
-            echo "Provided file $SERVER_LIST"
-            ;;
-        n) echo 'dry_run' ;;
-        s) echo 'run_as_sudo' ;;
-        v) 
-            VERBOSE_MODE='true'
-            echo 'Verbose mode ON'
-            ;;
+        f) SERVER_LIST="$OPTARG" ;;
+        d) DRY_RUN='true' ;;
+        s) SUDO='sudo' ;;
+        v) VERBOSE_MODE='true' ;;
         ?) usage ;;
     esac
 done
 
 # Remove options processed by 'getopts'
 shift "$(( OPTIND - 1 ))"
-
 if [[ "$#" -lt 1 ]]; then
     usage
 fi
-
 COMMAND="$@"
 
 if [[ ! -e "$SERVER_LIST" ]]; then
@@ -45,7 +43,18 @@ if [[ ! -e "$SERVER_LIST" ]]; then
 fi
 
 for SERVER in $(cat $SERVER_LIST); do
-    echo "Run command: $COMMAND on server $SERVER"
+    if [[ "$VERBOSE" = 'true' ]]; then
+        echo "Server: $SERVER"
+    fi
+
+    SSH_COMMAND="ssh -o ConnectTimeout=2 $SERVER $SUDO $COMMAND"
+    if [[ "$DRY_RUN" = 'true' ]]; then
+        echo "Command: $SSH_COMMAND"
+        continue
+    fi
+
+    # login into server & run command
+    echo 'EXECUTE REAL SSH_COMMAND'
 done
 
 exit 0
